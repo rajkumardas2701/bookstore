5bc0b5a9c6727a44cd077f4311c839570ea06e16
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-04-16 06:42:44 +0530

    Add Reacttable to display books and connect function

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,5 +1,33 @@
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import { connect } from 'react-redux';
+
 const BooksList = () => {
+  const columns = [{
+    Header: 'Book ID',
+    accessor: 'id',
+  }, {
+    Header: 'Title',
+    accessor: 'title',
+  }, {
+    Header: 'Category',
+    accessor: 'category',
+  }];
   return (
-          
-  )
-}
+    <div>
+      <ReactTable
+        data={data}
+        columns={columns}
+        defaultPageSize={2}
+        pageSizeOptions={[2, 4, 6]}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+
+};
+
+export default connect(mapStateToProps, null)(BooksList);
