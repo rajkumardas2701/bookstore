a0b4fa166593bc4b58965e358235f5405652fe9c
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-04-16 22:14:41 +0530

    Install react-redux package

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
-import 'react-table/react-table.css';
+// import 'react-table/react-table.css';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 
