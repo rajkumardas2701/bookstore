8f2be8e450cff708dd1339a4f8f3e77552f09838
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-04-19 00:59:58 +0530

    Add local state for title and category

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,25 +1,32 @@
+import { useState } from 'react';
+
 const BooksForm = () => {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const [state, setState] = useState({ title: '', category: '' });
+
+  const { title, category } = state;
 
   return (
-    <form>
+    <>
       <h1>Add a new book</h1>
-      <label htmlFor="book-title">
-        Title:
-        <input type="text" id="book-title" name="book-title" />
-      </label>
-      <label htmlFor="book-category">
-        Category:
-        <select id="book-category">
-          {
-            categories.map((category) => (
-              <option key={category} value={category}>{category}</option>
-            ))
-          }
-        </select>
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+      <form>
+        <label htmlFor="book-title">
+          Title:
+          <input type="text" id="book-title" name="book-title" />
+        </label>
+        <label htmlFor="book-category">
+          Category:
+          <select id="book-category">
+            {
+              CATEGORIES.map((category) => (
+                <option key={category} value={category}>{category}</option>
+              ))
+            }
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
 
